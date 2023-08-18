ddb6131c9ef0baa76ce1d7a68fb3c32549feb8f5
Alexandr <l4legenda@gmail.com>
2023-08-18 19:48:17 +0500

    feat: add peerjs

--- index.ts
+++ index.ts
@@ -7,9 +7,10 @@ import http from 'http';
 // import { parseStream, parseFile } from 'music-metadata';
 import { createRequire } from 'node:module';
 import bodyParser from 'body-parser';
+import { ExpressPeerServer } from 'peer';
+
 const require = createRequire(import.meta.url);
 
-const memoEval = memoize(eval);
 
 const app = express();
 
@@ -18,14 +19,7 @@ const GQL_SSL = process.env.GQL_SSL || 0;
 
 const toJSON = (data) => JSON.stringify(data, Object.getOwnPropertyNames(data), 2);
 
-const makeFunction = (code: string) => {
-  const fn = memoEval(code);
-  if (typeof fn !== 'function')
-  {
-    throw new Error("Executed handler's code didn't return a function.");
-  }
-  return fn;
-}
+
 
 const makeDeepClient = (token: string) => {
   if (!token) throw new Error('No token provided');
@@ -60,6 +54,12 @@ const requireWrapper = (id: string) => {
   return require(id);
 }
 
+const serverListen = app.listen(process.env.PORT);
+const options: any = { debug: true }
+const peerServer = ExpressPeerServer(serverListen, options);
+
+app.use('/peerjs', peerServer);
+
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.get('/healthz', (req, res) => {
@@ -68,40 +68,9 @@ app.get('/healthz', (req, res) => {
 app.post('/init', (req, res) => {
   res.json({});
 });
-app.post('/call', async (req, res) => {
-  try {
-    console.log('call body params', req?.body?.params);
-    const { jwt, code, data } = req?.body?.params || {};
-    const fn = makeFunction(code);
-    const deep = makeDeepClient(jwt);
-    const result = await fn({ data, deep, gql, require: requireWrapper }); // Supports both sync and async functions the same way
-    console.log('call result', result);
-    res.json({ resolved: result });
-  }
-  catch(rejected)
-  {
-    const processedRejection = JSON.parse(toJSON(rejected));
-    console.log('rejected', processedRejection);
-    res.json({ rejected: processedRejection });
-  }
-});
 
-app.use('/http-call', async (req, res, next) => {
-  try {
-    const options = decodeURI(`${req.headers['deep-call-options']}`) || '{}';
-    console.log('deep-call-options', options);
-    const { jwt, code, data } = JSON.parse(options as string);
-    const fn = makeFunction(code);
-    const deep = makeDeepClient(jwt);
-    await fn(req, res, next, { data, deep, gql, require: requireWrapper }); // Supports both sync and async functions the same way
-  }
-  catch(rejected)
-  {
-    const processedRejection = JSON.parse(toJSON(rejected));
-    console.log('rejected', processedRejection);
-    res.json({ rejected: processedRejection }); // TODO: Do we need to send json to client?
-  }
-});
 
-http.createServer({ maxHeaderSize: 10*1024*1024*1024 }, app).listen(process.env.PORT);
+http.createServer({ maxHeaderSize: 10*1024*1024*1024 }, app)
+
+
 console.log(`Listening ${process.env.PORT} port`);
